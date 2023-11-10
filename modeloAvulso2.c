188859bc9e0c50751358cec5a4154fd9d5bcabb2
Diego Santos <133829130+diegookys@users.noreply.github.com>
2023-11-10 20:54:17 -0300

    Create modeloAvulso2.c

---
+++ modeloAvulso2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+// alocacao estatica
+char vetor_estatico[5];
+
+  printf("preenchendo vetor estatico...\n");
+  for(int i = 0; i < 5; i++)
+{
+  scanf("%d", &vetor_estatico[i]);
+}
+
+
+// alocacao dinamica
+int* vetor_dinamico;
+vetor_dinamico = (int*) malloc(5*sizeof(int)); // 5 x tamanho de cada item (4 bytes)
+
+printf("preenchendo vetor dinamico...\n");
+for(int i = 0; i < 5; i++)
+{
+    scanf("%d", &vetor_dinamico[i]);
+}
+
+for(int i = 0; i < 5; i++);
+{
+    printf("vetor_estatico[%d] = %d\n", i, vetor_dinamico[i]);
+    printf("vetor_dinamico[%d] = %d\n", i, vetor_dinamico[i]);
+}
+  return 0;
+}
